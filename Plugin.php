a4725da884644092a870f65b268a0a715fd35bca
spanjaan <spanjaan@gmail.com>
2023-12-31 09:42:31 +0545

    Some Method Change

--- Plugin.php
+++ Plugin.php
@@ -3,28 +3,25 @@
 declare(strict_types=1);
 
 namespace SpAnjaan\BlogPortal;
-
-use Backend;
 use Event;
-use Exception;
+use Backend;
 use Backend\Controllers\Users as BackendUsers;
 use Backend\Facades\BackendAuth;
 use Backend\Models\User as BackendUser;
 use Backend\Widgets\Lists;
 use Cms\Classes\Controller;
-use Cms\Classes\Theme;
 use Winter\Blog\Controllers\Posts;
 use Winter\Blog\Models\Post;
 use SpAnjaan\BlogPortal\Behaviors\BlogPortalBackendUserModel;
 use SpAnjaan\BlogPortal\Behaviors\BlogPortalPostModel;
 use SpAnjaan\BlogPortal\Models\Comment;
 use SpAnjaan\BlogPortal\Models\Visitor;
-use Symfony\Component\Yaml\Yaml;
 use System\Classes\PluginBase;
+use Winter\Translate\FormWidgets\MLRichEditor;
 
 class Plugin extends PluginBase
 {
-    /**
+        /**
      * Required Extensions
      *
      * @var array
@@ -32,6 +29,11 @@ class Plugin extends PluginBase
     public $require = [
         'Winter.Blog'
     ];
+    
+    // Define constants for event names
+    private const MENU_EVENT = 'backend.menu.extendItems';
+    private const FORM_EVENT = 'backend.form.extendFields';
+    private const PAGE_EVENT = 'cms.page.end';
 
     /**
      * Returns information about this plugin.
@@ -56,13 +58,12 @@ class Plugin extends PluginBase
      */
     public function register()
     {
-
         // Extend available sorting options
-        Post::$allowedSortingOptions['spanjaan_blogportal_views asc'] = 'spanjaan.blogportal::lang.sorting.blogportal_views_asc';
-        Post::$allowedSortingOptions['spanjaan_blogportal_views desc'] = 'spanjaan.blogportal::lang.sorting.blogportal_views_desc';
-        Post::$allowedSortingOptions['spanjaan_blogportal_unique_views asc'] = 'spanjaan.blogportal::lang.sorting.blogportal_unique_views_asc';
-        Post::$allowedSortingOptions['spanjaan_blogportal_unique_views desc'] = 'spanjaan.blogportal::lang.sorting.blogportal_unique_views_desc';
-        Post::$allowedSortingOptions['spanjaan_blogportal_comments_count asc'] = 'spanjaan.blogportal::lang.sorting.blogportal_comments_count_asc';
+        Post::$allowedSortingOptions['spanjaan_blogportal_views asc']           = 'spanjaan.blogportal::lang.sorting.blogportal_views_asc';
+        Post::$allowedSortingOptions['spanjaan_blogportal_views desc']          = 'spanjaan.blogportal::lang.sorting.blogportal_views_desc';
+        Post::$allowedSortingOptions['spanjaan_blogportal_unique_views asc']    = 'spanjaan.blogportal::lang.sorting.blogportal_unique_views_asc';
+        Post::$allowedSortingOptions['spanjaan_blogportal_unique_views desc']   = 'spanjaan.blogportal::lang.sorting.blogportal_unique_views_desc';
+        Post::$allowedSortingOptions['spanjaan_blogportal_comments_count asc']  = 'spanjaan.blogportal::lang.sorting.blogportal_comments_count_asc';
         Post::$allowedSortingOptions['spanjaan_blogportal_comments_count desc'] = 'spanjaan.blogportal::lang.sorting.blogportal_comments_count_desc';
     }
 
@@ -73,8 +74,18 @@ class Plugin extends PluginBase
      */
     public function boot()
     {
-        // Add side menuts to Winter.Blog
-        Event::listen('backend.menu.extendItems', function ($manager) {
+        $this->extendBackendMenu();
+        $this->extendRichEditor();
+        $this->collectUniqueViews();
+        $this->implementCustomModels();
+
+        // Add other boot-time functionality if needed
+    }
+
+    private function extendBackendMenu()
+    {
+        // Add side menus to Winter.Blog
+        Event::listen(self::MENU_EVENT, function ($manager) {
             $manager->addSideMenuItems('Winter.Blog', 'blog', [
                 'spanjaan_blogportal_tags' => [
                     'label'         => 'spanjaan.blogportal::lang.model.tags.label',
@@ -100,16 +111,20 @@ class Plugin extends PluginBase
                 ]
             ]);
         });
+    }
 
+    private function extendRichEditor()
+    {
         // Extend Richeditor in blog content Field.
-        Event::listen('backend.form.extendFields', function ($widget) {
-            // Extend only the Blog\Posts controller & Extend only Blog\Post model
+        Event::listen(self::FORM_EVENT, function ($widget) {
+            // Check if the controller is an instance of \Winter\Blog\Controllers\Posts
+            // and the model is an instance of \Winter\Blog\Models\Post
             if (!($widget->getController() instanceof \Winter\Blog\Controllers\Posts
-                    && $widget->model instanceof \Winter\Blog\Models\Post)
-            ) {
+                && $widget->model instanceof \Winter\Blog\Models\Post)) {
                 return;
             }
 
+            // Continue with the rest of the code for extending fields
             $field = $widget->getField('content');
             if (class_exists('Winter\Translate\FormWidgets\MLRichEditor')) {
                 $field->config['widget'] = 'Winter\Translate\FormWidgets\MLRichEditor';
@@ -117,9 +132,12 @@ class Plugin extends PluginBase
                 $field->config['widget'] = 'Backend\FormWidgets\RichEditor';
             }
         });
+    }
 
+    private function collectUniqueViews()
+    {
         // Collect (Unique) Views
-        Event::listen('cms.page.end', function (Controller $ctrl) {
+        Event::listen(self::PAGE_EVENT, function (Controller $ctrl) {
             $pageObject = $ctrl->getPageObject();
             if (property_exists($pageObject, 'vars')) {
                 $post = $pageObject->vars['post'] ?? null;
@@ -156,8 +174,11 @@ class Plugin extends PluginBase
                 }
             }
         });
+    }
 
-        // Implement custom Models
+    private function implementCustomModels()
+    {
+        // Implement custom Models for Post and BackendUser
         Post::extend(function ($model) {
             $model->implement[] = BlogPortalPostModel::class;
         });
@@ -166,9 +187,9 @@ class Plugin extends PluginBase
             $model->implement[] = BlogPortalBackendUserModel::class;
         });
 
-
         // Extend Form Fields on Posts Controller
         Posts::extendFormFields(function ($form, $model, $context) {
+            // Check if the model is an instance of Post
             if (!$model instanceof Post) {
                 return;
             }
@@ -191,15 +212,14 @@ class Plugin extends PluginBase
                     'showSearch'    => false,
                     'span'          => 'left',
                     'options'       => [
-                        'open' => 'spanjaan.blogportal::lang.model.comments.post_mode.open',
-                        'restricted' => 'spanjaan.blogportal::lang.model.comments.post_mode.restricted',
-                        'private' => 'spanjaan.blogportal::lang.model.comments.post_mode.private',
-                        'closed' => 'spanjaan.blogportal::lang.model.comments.post_mode.closed',
+                        'open'          => 'spanjaan.blogportal::lang.model.comments.post_mode.open',
+                        'restricted'    => 'spanjaan.blogportal::lang.model.comments.post_mode.restricted',
+                        'private'       => 'spanjaan.blogportal::lang.model.comments.post_mode.private',
+                        'closed'        => 'spanjaan.blogportal::lang.model.comments.post_mode.closed',
                     ],
-                    'permissions'   => ['spanjaan.blogportal.comments.access_comments_settings']
+                    'permissions'       => ['spanjaan.blogportal.comments.access_comments_settings']
                 ],
             ]);
-
 
             // Add Tags Field
             $form->addTabFields([
@@ -216,39 +236,43 @@ class Plugin extends PluginBase
 
         // Extend List Columns on Posts Controller
         Posts::extendListColumns(function (Lists $list, $model) {
+            // Check if the model is an instance of Post
             if (!$model instanceof Post) {
                 return;
             }
 
+            // Add custom list columns
             $list->addColumns([
                 'spanjaan_blogportal_views' => [
-                    'label' => 'spanjaan.blogportal::lang.model.visitors.views',
-                    'type' => 'number',
-                    'select' => 'concat(winter_blog_posts.spanjaan_blogportal_views, " / ", winter_blog_posts.spanjaan_blogportal_unique_views)',
-                    'align' => 'left'
+                    'label'     => 'spanjaan.blogportal::lang.model.visitors.views',
+                    'type'      => 'number',
+                    'select'    => 'concat(winter_blog_posts.spanjaan_blogportal_views, " / ", winter_blog_posts.spanjaan_blogportal_unique_views)',
+                    'align'     => 'left'
                 ]
             ]);
         });
 
         // Add Posts Filter Scope
         Posts::extendListFilterScopes(function ($filter) {
+            // Add custom filter scope for tags
             $filter->addScopes([
                 'spanjaan_blogportal_tags' => [
-                    'label' => 'spanjaan.blogportal::lang.model.tags.label',
+                    'label'      => 'spanjaan.blogportal::lang.model.tags.label',
                     'modelClass' => 'SpAnjaan\BlogPortal\Models\Tag',
-                    'nameFrom' => 'slug',
-                    'scope' => 'FilterTags'
+                    'nameFrom'   => 'slug',
+                    'scope'      => 'FilterTags'
                 ]
             ]);
         });
 
         // Extend Backend Users Controller
         BackendUsers::extendFormFields(function ($form, $model, $context) {
+            // Check if the model is an instance of BackendUser
             if (!$model instanceof BackendUser) {
                 return;
             }
 
-            // Add Display Name
+            // Add custom form fields for BackendUser
             $form->addTabFields([
                 'spanjaan_blogportal_display_name' => [
                     'label'         => 'spanjaan.blogportal::lang.model.users.displayName',
@@ -282,14 +306,14 @@ class Plugin extends PluginBase
     public function registerComponents()
     {
         return [
-            \SpAnjaan\BlogPortal\Components\PostsByAuthor::class => 'blogportalPostsByAuthor',
+            \SpAnjaan\BlogPortal\Components\PostsByAuthor::class       => 'blogportalPostsByAuthor',
             \SpAnjaan\BlogPortal\Components\PostsByCommentCount::class => 'blogportalPostsByCommentCount',
-            \SpAnjaan\BlogPortal\Components\PostsByDate::class => 'blogportalPostsByDate',
-            \SpAnjaan\BlogPortal\Components\PostsByTag::class => 'blogportalPostsByTag',
-            \SpAnjaan\BlogPortal\Components\CommentList::class => 'blogportalCommentList',
-            \SpAnjaan\BlogPortal\Components\CommentSection::class => 'blogportalCommentSection',
-            \SpAnjaan\BlogPortal\Components\Tags::class => 'blogportalTags',
-            \SpAnjaan\BlogPortal\Components\PopularPosts::class => 'popularPosts',
+            \SpAnjaan\BlogPortal\Components\PostsByDate::class         => 'blogportalPostsByDate',
+            \SpAnjaan\BlogPortal\Components\PostsByTag::class          => 'blogportalPostsByTag',
+            \SpAnjaan\BlogPortal\Components\CommentList::class         => 'blogportalCommentList',
+            \SpAnjaan\BlogPortal\Components\CommentSection::class      => 'blogportalCommentSection',
+            \SpAnjaan\BlogPortal\Components\Tags::class                => 'blogportalTags',
+            \SpAnjaan\BlogPortal\Components\PopularPosts::class        => 'popularPosts',
         ];
     }
 
@@ -302,30 +326,30 @@ class Plugin extends PluginBase
     {
         return [
             'spanjaan.blogportal.comments' => [
-                'tab'   => 'winter.blog::lang.blog.tab',
-                'label' => 'spanjaan.blogportal::lang.permissions.access_comments',
-                'comment' => 'spanjaan.blogportal::lang.permissions.access_comments_comment',
+                'tab'       => 'winter.blog::lang.blog.tab',
+                'label'     => 'spanjaan.blogportal::lang.permissions.access_comments',
+                'comment'   => 'spanjaan.blogportal::lang.permissions.access_comments_comment',
             ],
             'spanjaan.blogportal.comments.access_comments_settings' => [
-                'tab'   => 'winter.blog::lang.blog.tab',
-                'label' => 'spanjaan.blogportal::lang.permissions.manage_post_settings'
+                'tab'       => 'winter.blog::lang.blog.tab',
+                'label'     => 'spanjaan.blogportal::lang.permissions.manage_post_settings'
             ],
             'spanjaan.blogportal.comments.moderate_comments' => [
-                'tab'   => 'winter.blog::lang.blog.tab',
-                'label' => 'spanjaan.blogportal::lang.permissions.moderate_comments'
+                'tab'       => 'winter.blog::lang.blog.tab',
+                'label'     => 'spanjaan.blogportal::lang.permissions.moderate_comments'
             ],
             'spanjaan.blogportal.comments.delete_comments' => [
-                'tab'   => 'winter.blog::lang.blog.tab',
-                'label' => 'spanjaan.blogportal::lang.permissions.delete_commpents'
+                'tab'       => 'winter.blog::lang.blog.tab',
+                'label'     => 'spanjaan.blogportal::lang.permissions.delete_comments'
             ],
             'spanjaan.blogportal.tags' => [
-                'tab'   => 'winter.blog::lang.blog.tab',
-                'label' => 'spanjaan.blogportal::lang.permissions.access_tags',
-                'comment' => 'spanjaan.blogportal::lang.permissions.access_tags_comment',
+                'tab'       => 'winter.blog::lang.blog.tab',
+                'label'     => 'spanjaan.blogportal::lang.permissions.access_tags',
+                'comment'   => 'spanjaan.blogportal::lang.permissions.access_tags_comment',
             ],
             'spanjaan.blogportal.tags.promoted' => [
-                'tab'   => 'winter.blog::lang.blog.tab',
-                'label' => 'spanjaan.blogportal::lang.permissions.promote_tags'
+                'tab'       => 'winter.blog::lang.blog.tab',
+                'label'     => 'spanjaan.blogportal::lang.permissions.promote_tags'
             ]
         ];
     }
@@ -371,24 +395,24 @@ class Plugin extends PluginBase
     {
         return [
             \SpAnjaan\BlogPortal\ReportWidgets\CommentsList::class => [
-                'label' => 'spanjaan.blogportal::lang.widgets.comments_list.label',
-                'context' => 'dashboard',
-                'permission' => [
+                'label'         => 'spanjaan.blogportal::lang.widgets.comments_list.label',
+                'context'       => 'dashboard',
+                'permission'    => [
                     'winter.blog.access_other_posts',
                     'spanjaan.blogportal.comments'
                 ]
             ],
             \SpAnjaan\BlogPortal\ReportWidgets\PostsList::class => [
-                'label' => 'spanjaan.blogportal::lang.widgets.posts_list.label',
-                'context' => 'dashboard',
-                'permission' => [
+                'label'         => 'spanjaan.blogportal::lang.widgets.posts_list.label',
+                'context'       => 'dashboard',
+                'permission'    => [
                     'winter.blog.access_other_posts'
                 ]
             ],
             \SpAnjaan\BlogPortal\ReportWidgets\PostsStatistics::class => [
-                'label' => 'spanjaan.blogportal::lang.widgets.posts_statistics.label',
-                'context' => 'dashboard',
-                'permission' => [
+                'label'         => 'spanjaan.blogportal::lang.widgets.posts_statistics.label',
+                'context'       => 'dashboard',
+                'permission'    => [
                     'winter.blog.access_other_posts'
                 ]
             ],
